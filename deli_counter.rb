0b32c1862499a7de5a663c5923ee8b59bdef935e
Abraham Moreno <adiaz2821@gmail.com>
2019-11-01 11:57:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,4 @@
 # Write your code here.
-=begin def line(katz_deli)
-  if katz_deli.length > 0
-    string = "The line is currently:" 
-    katz_deli.each_with_index do |name, index| 
-    string +=" #{index + 1}. #{name}"
-    end
-    puts string
-     
-  else 
-   puts "The line is currently empty."
-  end 
-end 
-=end
-
 
 def line(katz_deli)
   line_array = []
