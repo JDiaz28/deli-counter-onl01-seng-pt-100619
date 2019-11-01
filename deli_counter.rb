d3be647f8e8885aec37ea9f7799ced4b6706b3e8
Abraham Moreno <adiaz2821@gmail.com>
2019-11-01 11:46:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,11 +14,7 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli.push(name)
-        puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
-      end
-  else 
-     puts "The line is currently empty."
-  end
+     puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
 end 
 
 def now_serving(array)
